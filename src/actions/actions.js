bb1c7817df27767709b3dff8e1a407a5aa0ca9bf
Boris A. Burkov <vasjaforutube@gmail.com>
2019-05-04 23:54:40 +0100

    Created a stub of redux project.

---
+++ src/actions/actions.js
@@ -0,0 +1,23 @@
+import * as types from './actionTypes';
+import routes from 'services/routes.jsx';
+
+
+export function receiveStuff(json) {
+  return {type: types.RECEIVE_STUFF, stuff: json.stuff};
+}
+
+export function fetchStuff() {
+  return dispatch => {
+    return fetch(routes.facets(), {
+      method: 'GET',
+      mode: 'cors',
+      credentials: 'include',
+      headers: {
+        'x-api-key': apiKey,
+        'Accept': 'application/json'
+      }
+    })
+    .then(response => response.json())
+    .then(json => dispatch(receiveStuff(json)));
+  };
+}
--- src/app.jsx
+++ src/app.jsx
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter as Router, Route, Link, browserHistory} from 'react-router-dom';
 
 import SequenceSearch from 'components/SequenceSearch/index.jsx';
+import configureStore from 'store/configureStore.js';
 
+
+const store = configureStore();
 
 ReactDOM.render(
   <SequenceSearch/>,
