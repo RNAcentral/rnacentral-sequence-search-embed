41b752dbc18d211e11ebfb27d184cabefd3efe60
Boris A. Burkov <vasjaforutube@gmail.com>
2019-05-05 23:44:04 +0100

    Passed the store to the SequenceSearch component.

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter as Router, Route, Link, browserHistory} from 'react-router-dom';
 
 import SequenceSearch from 'components/SequenceSearch/index.jsx';
 import configureStore from 'store/configureStore.js';
@@ -9,6 +8,6 @@ import configureStore from 'store/configureStore.js';
 const store = configureStore();
 
 ReactDOM.render(
-  <SequenceSearch/>,
+  <SequenceSearch store={store}/>,
   document.querySelector('div#main')
 );
