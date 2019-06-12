eac10d0223709d27e4fec284e22132a0daa883b2
burkov <burkov@ebi.ac.uk>
2019-06-12 12:33:43 +0300

    Got rid of unused EBI theme scripts.

--- src/containers/SequenceSearch/index.jsx
+++ src/containers/SequenceSearch/index.jsx
@@ -1,20 +1,8 @@
 import React from 'react';
-import 'ebi-framework/js/script.js';
-import 'foundation-sites/dist/js/foundation.js';
-import 'ebi-framework/js/foundationExtendEBI.js';
 import 'jquery/dist/jquery.js';
-
-// import 'foundation-sites/dist/css/foundation.css';  // clash with ebi-framework1.3: header 'display: block/flexbox'
-// import 'ebi-framework/css/ebi-global.css';
-// import 'ebi-framework/css/theme-light.css';
-// import 'EBI-Icon-fonts/fonts.css';
-// import 'animate.css/animate.min.css';
 
 import Results from 'containers/SequenceSearch/components/Results/index.jsx';
 import SearchForm from 'containers/SequenceSearch/components/SearchForm/index.jsx';
-import * as actions from 'actions/actionTypes';
-import * as actionCreators from 'actions/actions';
-import routes from "services/routes.jsx";
 import {connect} from "react-redux";
 
 
@@ -29,12 +17,6 @@ class SequenceSearch extends React.Component {
       <Results key={`results`} />
     ]
   }
-
-  componentDidMount() {
-    $(document).foundation();
-    $(document).foundationExtendEBI();
-  }
-
 }
 
 function mapStateToProps(state) {
