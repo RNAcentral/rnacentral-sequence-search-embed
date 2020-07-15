882296b5a2e05513724491b033203baef0221c03
carlosribas <caduribas@gmail.com>
2020-07-15 14:57:26 +0100

    Fix SnoDB link

--- src/containers/SequenceSearch/components/Results/components/Hit.jsx
+++ src/containers/SequenceSearch/components/Results/components/Hit.jsx
@@ -9,13 +9,6 @@ import { FaCheckCircle } from 'react-icons/fa';
 
 class Hit extends React.Component {
   render() {
-    const expertDb = this.props.entry.fields && this.props.entry.fields.expert_db ? this.props.entry.fields.expert_db : [];
-    const newExpertDb = expertDb.map((item) => {
-      return item.toLowerCase();
-    });
-    const showExpertDb = images.filter(({src, title}) => newExpertDb.includes(title));
-    const database = this.props.databases;
-    const exactMatchUrsId = this.props.exactMatchUrsId.indexOf(this.props.entry.rnacentral_id) > -1 ? <FaCheckCircle style={{color: '#3c763d', verticalAlign: '-1px'}}/> : '';
     let seqTitleStyle = {
       color: this.props.customStyle && this.props.customStyle.linkColor ? this.props.customStyle.linkColor : "#337ab7",
       fontSize: this.props.customStyle && this.props.customStyle.seqTitleSize ? this.props.customStyle.seqTitleSize : "20px",
@@ -24,15 +17,30 @@ class Hit extends React.Component {
       color: this.props.customStyle && this.props.customStyle.seqInfoColor ? this.props.customStyle.seqInfoColor : "",
       fontSize: this.props.customStyle && this.props.customStyle.seqInfoSize ? this.props.customStyle.seqInfoSize : "",
     };
-    const link = database.length !== 0 && this.props.entry.fields && this.props.entry.fields.url && this.props.entry.fields.url.length ? this.props.entry.fields.url.filter((item) => item.includes(database[0].toLowerCase())) : ''
+
+    // show Expert DBs logos
+    const expertDb = this.props.entry.fields && this.props.entry.fields.expert_db ? this.props.entry.fields.expert_db : [];
+    const newExpertDb = expertDb.map((item) => {
+      return item.toLowerCase();
+    });
+    const showExpertDb = images.filter(({src, title}) => newExpertDb.includes(title));
+    const exactMatchUrsId = this.props.exactMatchUrsId.indexOf(this.props.entry.rnacentral_id) > -1 ? <FaCheckCircle style={{color: '#3c763d', verticalAlign: '-1px'}}/> : '';
+
+    // check database used. Empty array means RNAcentral
+    let database = this.props.databases.length !== 0 ? this.props.databases[0].toLowerCase() : "";
+    // there isn’t always a relationship between the DB name and the url.
+    if (database === "snodb") {
+      database = "scottgroup";
+    }
+    const link = database && this.props.entry.fields && this.props.entry.fields.url && this.props.entry.fields.url.length ? this.props.entry.fields.url.filter((item) => item.includes(database)) : ''
 
     return (
       <li>
         {exactMatchUrsId} <a className="custom-link" style={seqTitleStyle} href={link ? link[0] : `https://rnacentral.org/rna/${this.props.entry.rnacentral_id}`} target='_blank'>
           {this.props.entry.description}
         </a>
-        {database.length === 0 && <div className="text-muted mt-2" style={seqInfoStyle}>{ this.props.entry.rnacentral_id } {showExpertDb.map((db, index) => <img key={index} className="ml-2 desaturate" src={db.src} style={{height: "16px"}} />)}</div>}
-        {database.length === 0 ? '' : <div className="text-muted mt-2" style={seqInfoStyle}>{this.props.entry.target_length} nucleotides</div>}
+        {database === "" ? <div className="text-muted mt-2" style={seqInfoStyle}>{ this.props.entry.rnacentral_id } {showExpertDb.map((db, index) => <img key={index} className="ml-2 desaturate" src={db.src} style={{height: "16px"}} />)}</div>
+            : <div className="text-muted mt-2" style={seqInfoStyle}>{this.props.entry.target_length} nucleotides</div>}
         <div className={this.props.detailsCollapsed ? 'detail-collapsed' : 'mt-1'}>
           <span className="detail">E-value: { this.props.entry.e_value.toExponential() }</span>
           <span className="detail">Identity: { `${parseFloat(this.props.entry.identity).toFixed(2)}%`}</span>
