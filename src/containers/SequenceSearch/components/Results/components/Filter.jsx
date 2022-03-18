af54f00c808191ee755e983b5de6c082226dc265
carlosribas <caduribas@gmail.com>
2022-03-18 18:08:31 +0000

    Update release number

--- src/containers/SequenceSearch/components/Results/components/Filter.jsx
+++ src/containers/SequenceSearch/components/Results/components/Filter.jsx
@@ -119,7 +119,7 @@ class Filter extends Component {
       "homepage": url,
       "title": title,
       "description": description,
-      "rnacentral_version": "v19",
+      "rnacentral_version": "v20",
       "licenses": [{
         "name": "CC0",
         "path": "https://creativecommons.org/share-your-work/public-domain/cc0/",
