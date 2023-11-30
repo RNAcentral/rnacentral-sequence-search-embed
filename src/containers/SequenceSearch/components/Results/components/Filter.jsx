b4b49cb48c354f971ae3cf031876eec4111adb20
carlosribas <caduribas@gmail.com>
2023-11-30 11:40:07 +0000

    Update rnacentral_version

--- src/containers/SequenceSearch/components/Results/components/Filter.jsx
+++ src/containers/SequenceSearch/components/Results/components/Filter.jsx
@@ -119,7 +119,7 @@ class Filter extends Component {
       "homepage": url,
       "title": title,
       "description": description,
-      "rnacentral_version": "v22",
+      "rnacentral_version": "v23",
       "licenses": [{
         "name": "CC0",
         "path": "https://creativecommons.org/share-your-work/public-domain/cc0/",
